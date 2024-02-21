8f60b59da94e0be454d2c4fe537f2e94310fe70c
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-02-21 22:19:49 +0100

    Completed strings

--- task-strings.js
+++ task-strings.js
@@ -6,10 +6,11 @@
 // console.log(parametrize("Javascript is the best"));
 // "javascript-is-the-best"
 
-// Your code:
 const parametrize = (input) => {
-
+    return input.toLowerCase().replaceAll(" ", "-");
 };
+
+console.log(parametrize("Javascript is the best"));
 
 // 2 =================================
 // Write function that takes object and return sentence
@@ -17,17 +18,19 @@ const parametrize = (input) => {
 // giveSentenceForPerson({name: "Obi-wan", position: "Jedi", age: 40, location: "Starwars universe"})
 // ➞ Hello Obi-wan from Starwars universe! It is nice to meet you! You look awesome for your 40, young Jedi!
 
-// Your code:
 const giveSentenceForPerson = (obj) => {
-
+    return `Hello ${obj.name} from ${obj.location}! It is nice to meet you! You look awesome for your ${obj.age}, young ${obj.position}!`;
 };
+
+console.log(giveSentenceForPerson({name: "Obi-wan", position: "Jedi", age: 40, location: "Starwars universe"}));
 
 // 3 =================================
 // Write function that takes string and replace all `a`, `e`, `i`, `o` with *
 // Example:
 // replace("Hello Javascript") ➞ "H*ll* J*v*scr*pt"
 
-// Your code:
 const replace = (input) => {
-
+    return input.replaceAll(/[aeio]/g, "*");
 };
+
+console.log(replace("Hello Javascript"));
