29f168b721c754583447465ab2d69b4199f031c9
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-02-21 21:07:37 +0100

    Completed cycles

--- task-cycles.js
+++ task-cycles.js
@@ -5,10 +5,18 @@
 // arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
 // arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
 
-// Your code:
 const arrayOfMultiples = (num, length) => {
-    // ... write code ...
+    let numbers = [];
+    for (let index = 0; index < length; index++) {
+        numbers.push(num * (index + 1));
+    }
+
+    return numbers;
 };
+
+console.log(arrayOfMultiples(7, 5));
+console.log(arrayOfMultiples(12, 10));
+console.log(arrayOfMultiples(17, 6));
 
 // 2 =================================
 // Change direction of array
@@ -18,10 +26,13 @@ const arrayOfMultiples = (num, length) => {
 // changeDirection([]) ➞ []
 // changeDirection([1, 2]) ➞ [2, 1]
 
-// Your code:
 const changeDirection = (array) => {
-// ... write code ...
+    return array.reverse();
 };
+
+console.log(changeDirection([0, 1, 2, 3]));
+console.log(changeDirection([]));
+console.log(changeDirection([1, 2]));
 
 // 3 =================================
 // Create function that takes two arrays and return object with two keys - bigger array, sum all numbers
@@ -29,7 +40,12 @@ const changeDirection = (array) => {
 // biggerArray([1,2,3,4,5], [50,50]) ➞ { array: [50,50], sum: 100 }
 // biggerArray([1,2,3], [2,3,4]) ➞ { array: [2,3,4], sum: 9 }
 
-// Your code:
 const biggerArray = (array1, array2) => {
-// ... write code ...
+    let sum1 = array1.reduce((a, b) => a + b, 0);
+    let sum2 = array2.reduce((a, b) => a + b, 0);
+
+    return sum1 > sum2 ? { array: array1, sum: sum1 } : { array: array2, sum: sum2 };
 };
+
+console.log(biggerArray([1, 2, 3, 4, 5], [50, 50]));
+console.log(biggerArray([1, 2, 3], [2, 3, 4]));
